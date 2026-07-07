fb585c287a9d6cc19a64794f7209756d11759901
aryav <104564110+aryav-hash@users.noreply.github.com>
2026-07-07 22:08:18 +0530

    feat: Wrote the client.py file where we are connecting to the testnet client and setting the testnet url. Used a class to define the variables, so that it can be used by other files.

---
+++ bot/client.py
@@ -0,0 +1,22 @@
+import os
+import logging
+from dotenv import load_dotenv
+from binance.client import Client
+
+logger = logging.getLogger(__name__)
+
+class BinanceTestnetClient:
+    def __init__(self):
+        load_dotenv()
+        api_key = os.getenv("API_KEY")
+        api_secret = os.getenv("API_SECRET")
+
+        logger.info("Making connection to Binance ....")
+
+        self.client = Client(api_key, api_secret, testnet=True)
+        self.client.API_URL = "https://testnet.binancefuture.com"
+
+        logger.info("Connected to the Binance Future Testnet.")
+
+def get_client(self):
+    return self.client
